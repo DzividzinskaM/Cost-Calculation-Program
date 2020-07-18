519e296755778c0880d3ee69b3d31b089791b5dc
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-07-19 00:14:48 +0300

    add storage form

---
+++ Cost-Calculation-Program/Cost-Calculation-Program/StorageForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Cost_Calculation_Program
+{
+    partial class StorageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NewProductBtn = new System.Windows.Forms.Button();
+            this.ProductBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BackBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Ivory;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(208, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 74);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Склад";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NewProductBtn
+            // 
+            this.NewProductBtn.BackColor = System.Drawing.Color.SandyBrown;
+            this.NewProductBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NewProductBtn.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.NewProductBtn.FlatAppearance.BorderSize = 4;
+            this.NewProductBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NewProductBtn.Font = new System.Drawing.Font("Sitka Banner", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NewProductBtn.ForeColor = System.Drawing.Color.Maroon;
+            this.NewProductBtn.Location = new System.Drawing.Point(181, 116);
+            this.NewProductBtn.Name = "NewProductBtn";
+            this.NewProductBtn.Size = new System.Drawing.Size(269, 54);
+            this.NewProductBtn.TabIndex = 1;
+            this.NewProductBtn.Text = "Новий продукт";
+            this.NewProductBtn.UseVisualStyleBackColor = false;
+            // 
+            // ProductBtn
+            // 
+            this.ProductBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProductBtn.BackColor = System.Drawing.Color.SandyBrown;
+            this.ProductBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ProductBtn.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.ProductBtn.FlatAppearance.BorderSize = 4;
+            this.ProductBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProductBtn.Font = new System.Drawing.Font("Sitka Banner", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ProductBtn.ForeColor = System.Drawing.Color.Maroon;
+            this.ProductBtn.Location = new System.Drawing.Point(205, 198);
+            this.ProductBtn.Name = "ProductBtn";
+            this.ProductBtn.Size = new System.Drawing.Size(228, 58);
+            this.ProductBtn.TabIndex = 2;
+            this.ProductBtn.Text = "Продукт";
+            this.ProductBtn.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Sitka Banner", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(139, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(0, 143);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // BackBtn
+            // 
+            this.BackBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BackBtn.BackColor = System.Drawing.Color.SandyBrown;
+            this.BackBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BackBtn.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.BackBtn.FlatAppearance.BorderSize = 4;
+            this.BackBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BackBtn.Font = new System.Drawing.Font("Sitka Banner", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BackBtn.ForeColor = System.Drawing.Color.Maroon;
+            this.BackBtn.Location = new System.Drawing.Point(205, 292);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(228, 58);
+            this.BackBtn.TabIndex = 4;
+            this.BackBtn.Text = "Назад";
+            this.BackBtn.UseVisualStyleBackColor = false;
+            this.BackBtn.Click += new System.EventHandler(this.BackBtn_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.AutoSize = true;
+            this.CloseBtn.BackColor = System.Drawing.Color.Ivory;
+            this.CloseBtn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CloseBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseBtn.Font = new System.Drawing.Font("Sitka Subheading", 20F, System.Drawing.FontStyle.Bold);
+            this.CloseBtn.ForeColor = System.Drawing.Color.Maroon;
+            this.CloseBtn.Location = new System.Drawing.Point(573, -3);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(46, 51);
+            this.CloseBtn.TabIndex = 6;
+            this.CloseBtn.Text = "X";
+            this.CloseBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // StorageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cost_Calculation_Program.Properties.Resources.storage_background;
+            this.ClientSize = new System.Drawing.Size(615, 388);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.BackBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ProductBtn);
+            this.Controls.Add(this.NewProductBtn);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StorageForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StorageForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button NewProductBtn;
+        private System.Windows.Forms.Button ProductBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BackBtn;
+        private System.Windows.Forms.Label CloseBtn;
+    }
+}
