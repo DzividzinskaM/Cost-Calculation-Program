2f72250dae3f87725747fdbc4c9ed59e2e130bbe
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-07-23 18:22:14 +0300

    add product class

---
+++ Cost-Calculation-Program/Cost-Calculation-Lib/ProductDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cost_Calculation_Lib
+{
+    class ProductDB
+    {
+    }
+}
