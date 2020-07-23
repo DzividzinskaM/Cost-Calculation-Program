c5c605530a33bfa60945c24fe005bfb4c4206726
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-07-23 18:21:58 +0300

    add db files

---
+++ Cost-Calculation-Program/Cost-Calculation-Lib/DB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cost_Calculation_Lib
+{
+    public class DB
+    {
+        private static DB instance;
+
+        private static string name = "SqlProvider";
+
+        public static string connectionStr;
+
+
+        private DB()
+        {
+            GetConnectionString();
+        }
+
+        public static DB GetDBInstance()
+        {
+            if (instance == null)
+                instance = new DB();
+            return instance;
+        }
+
+        public static string GetConnectionString()
+        {
+
+            string returnValue = null;
+
+            if (ConfigurationManager.ConnectionStrings[name] != null)
+                returnValue = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+            return returnValue;
+        }
+    }
+}
