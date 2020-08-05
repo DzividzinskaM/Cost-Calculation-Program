229478eb0beaf009ec734fcc8f900808dca70a85
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-08-05 23:32:28 +0300

    add successfulAdding form

---
+++ Cost-Calculation-Program/Cost-Calculation-Program/SuccessfulAdding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cost_Calculation_Program
+{
+    public partial class SuccessfulAdding : Form
+    {
+        public SuccessfulAdding()
+        {
+            InitializeComponent();
+        }
+
+        private void OkBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AddingProductForm addingProductForm = new AddingProductForm();
+            addingProductForm.Show();
+        }
+    }
+}
