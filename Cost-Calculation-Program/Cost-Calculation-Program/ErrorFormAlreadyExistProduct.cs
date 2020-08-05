e2d0b204157644bb138cab62ed864bd849e97d1a
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-08-05 23:32:59 +0300

    add error form

---
+++ Cost-Calculation-Program/Cost-Calculation-Program/ErrorFormAlreadyExistProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cost_Calculation_Program
+{
+    public partial class ErrorFormAlreadyExistProduct : Form
+    {
+        public ErrorFormAlreadyExistProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void OkBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AddingProductForm addingProductForm = new AddingProductForm();
+            addingProductForm.Show();
+        }
+    }
+}
